149e24b517b9ea037ef789257001c7d409cda155
Anderson-ALGO <71199356+Anderson-ALGO@users.noreply.github.com>
2020-10-12 11:14:54 -0500

    Create Python.py

---
+++ PineScript/Operaciones/Python.py
@@ -0,0 +1,56 @@
+#Operaciones + Lab
+
+#07-10-2020
+
+#Variables
+
+x = [1, 3, 2, 7, 6, 19]
+
+#Numero de Datos Total
+
+y = len(x)
+
+#La suma de todos los valores
+
+z = 0
+
+for indice in x:
+    z = z + indice
+   
+r = z/y   
+ 
+print("Suma De Todos Los Datos : {} \nNumero De Datos : {}\nPromedio : {} " .format(z, y, r))
+
+#12-10-2020
+
+
+    
+r_ = 0
+
+for desv in x:
+    r_ = ((desv - r)**2) + r_
+
+des  = (r_ / (y - 1))
+des_ = (des**(0.5))
+
+print("Varianza : {} \nDesviación : {}".format(des,des_))
+
+
+def Media(x):
+    y = len(x)
+    z = 0
+    for indice in x:
+        z = z + indice 
+    r = z/y    
+    r_ = 0
+    for desv in x:
+        r_ = ((desv - r)**2) + r_
+    des  = (r_ / (y - 1))
+    des_ = (des**(0.5))
+    
+    return z, y, r, des, des_
+    
+anderson = [2,5,9,8,7,6,3,5]
+
+z, y, r, des, des_ = Media(anderson)
+print("Suma De Todos Los Datos : {} \nNumero De Datos : {}\nPromedio : {}\nVarianza : {} \nDesviación : {}".format(z, y, r, des, des_))
