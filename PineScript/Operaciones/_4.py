9f8d9bdead8deb4dc394c7d303040210304bfc95
Anderson-ALGO <71199356+Anderson-ALGO@users.noreply.github.com>
2020-11-04 10:17:01 -0500

    Update _4.py

--- PineScript/Operaciones/_4.py
+++ PineScript/Operaciones/_4.py
@@ -3,25 +3,38 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
+import numpy as np
 
-amd = pd.read_excel("AMD.xlsx")
-a =  amd ["Close"]
+start = dt.datetime(2010, 6, 5)
+end = dt.datetime(2020, 10, 5)
 
-google = pd.read_excel("GOOGL.xlsx")
-b =  google ["Close"]
+firm = "EURUSD=X"
+amd = web.DataReader(firm, "yahoo", start, end)
+a = amd["Close"]
 
-adobe = pd.read_excel("ADBE.xlsx")
-c = adobe ["Close"]
+firm1 = "GBPUSD=X"
+appl = web.DataReader(firm1, "yahoo", start, end)
+b = appl["Close"]
+
+firm2 = "USDJPY=X"
+a_ = web.DataReader(firm2, "yahoo", start, end)
+c = a_["Close"]
+
+print(a.describe())
+print(b.describe())
+print(c.describe())
 
 from mpl_toolkits.mplot3d import axes3d
 
-def grafico_3d (x, y, z,x_lable, y_label, z_label):
+
+def grafico_3d(x, y, z, x_label, y_label, z_label):
     fig = plt.figure()
-    plt.ax1 = fig.add_subplot(111, projection = "3d")
+    plt.ax1 = fig.add_subplot(111, projection="3d")
     plt.ax1.scatter(x, y, z)
     plt.ax1.set_xlabel(x_label)
     plt.ax1.set_ylabel(y_label)
     plt.ax1.set_zlabel(z_label)
     plt.show()
-        
-grafico_3d(a, b, c, "AMD", "GOOGL", "ADOBE")
+
+
+grafico_3d(a, b, c, firm, firm1, firm2)
